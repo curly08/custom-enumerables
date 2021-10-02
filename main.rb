7c03d56268799aaff96b183a019e4cd166ab42bb
curly08 <mattynoakes08@gmail.com>
2021-10-01 22:19:28 -0400

    Add my_each method

--- main.rb
+++ main.rb
@@ -1,10 +1,21 @@
 module Enumerable
   def my_each
-
+    return to_enum(:my_each) unless block_given?
+    
+    for k, v in self
+      yield k, v
+    end
   end
 end
 
-puts "my_each vs. each"
+include Enumerable
+
+puts "Arrays: my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
-numbers.my_each  { |item| puts item }
+p numbers.my_each  #{ |item| puts item }
 numbers.each  { |item| puts item }
+
+puts "Hashes: my_each vs. each"
+fruit = {a: 'apples', b: 'bananas', c: 'cookies'}
+p fruit.my_each  { |k, v| puts v }
+p fruit.each  { |k, v| puts v }
