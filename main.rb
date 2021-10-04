d05981fe96442d5b68343e56dec5eab86821b7f4
curly08 <mattynoakes08@gmail.com>
2021-10-04 12:30:19 -0400

    Add my_select method

--- main.rb
+++ main.rb
@@ -12,11 +12,29 @@ module Enumerable
     return to_enum(:my_each_with_index) unless block_given?
 
     i = 0
-    self.my_each do |elem|
+    for elem in self
       yield elem, i
       i += 1
     end
     self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+    
+    case self
+    when Array
+      result = Array.new
+      self.my_each do |elem|
+        result.push(elem) if yield(elem)
+      end
+    when Hash
+      result = Hash.new
+      self.my_each do |elem|
+        result[elem[0]] = elem[1] if yield(elem)
+      end
+    end
+    result
   end
 end
 
@@ -24,10 +42,10 @@ include Enumerable
 
 puts "Arrays: my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
-p numbers.my_each_with_index  { |item, index| puts "Index: #{index} Value: #{item}" }
-p numbers.each_with_index  { |item, index| puts item }
+p numbers.my_select  { |num| num.odd? }
+p numbers.select { |num| num.odd? }
 
-puts "Hashes: my_each vs. each"
-fruit = {a: 'apples', b: 'bananas', c: 'cookies'}
-p fruit.my_each_with_index { |k, i| puts "Index: #{i},#{k} => " }
-p fruit.each_with_index { |k, i| puts "Index: #{i},#{k} => " }
+puts "\nHashes: my_each vs. each"
+fruit = {a: 5, b: 6, c: 3}
+p fruit.my_select { |k, v| v == 'cookies' }
+p fruit.select { |k, v| v == 'cookies' }
